f7c8d96b449be02529adda68d2a4165151ba845d
Max Arnold <arnoldmx@mail.uc.edu>
2025-03-27 11:36:32 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SayHelloMethod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHello("Max", "Arnold");
+        }
+        /// <summary>
+        /// Print Hello World to the console.
+        /// </summary>
+        /// <param name="= first">The first name</param>
+        /// <param name=" last">The last name</param>
+        static void SayHello(String first, String last)
+        {
+            Console.WriteLine("Hello" + " " + first + " " + last);
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("arnoldmx_InClass2.27.2025 GitHub Assignment")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("arnoldmx_InClass2.27.2025 GitHub Assignment")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0ce5b53c-5e10-410c-9ee8-2cecd9617eb0")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
